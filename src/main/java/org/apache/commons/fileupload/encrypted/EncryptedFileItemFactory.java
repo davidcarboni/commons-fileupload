1b49ed4a98dfe317ead4c71afe4f496f10a1d2ff
David Carboni <david@carboni.io>
2018-05-05 12:46:12 +0100

    Javadoc updated to minimise differences with DiskFileItemFactory.

--- src/main/java/org/apache/commons/fileupload/encrypted/EncryptedFileItemFactory.java
+++ src/main/java/org/apache/commons/fileupload/encrypted/EncryptedFileItemFactory.java
@@ -8,9 +8,55 @@ import org.apache.commons.io.FileCleaningTracker;
 import java.io.File;
 
 /**
- * This class encrypts uploaded data if they are written to disk.
+ * <p>A {@link org.apache.commons.fileupload.FileItemFactory}
+ * implementation providing transparent encryption of uploaded data.
+ * This implementation creates encrypted
+ * {@link org.apache.commons.fileupload.FileItem} instances which keep their
+ * content either in memory, for smaller items, or in a temporary file on disk,
+ * for larger items. The size threshold, above which content will be stored on
+ * disk, is configurable, as is the directory in which temporary files will be
+ * created.</p>
  *
- * Based on {@code org.apache.commons.fileupload.disk.DiskFileItemFactory} 1564788 2014-02-05 14:36:41Z markt $ - {@code commons-fileupload 1.4-SNAPSHOT}.
+ * <p>If not otherwise configured, the default configuration values are as
+ * follows:</p>
+ * <ul>
+ *   <li>Size threshold is 10KB.</li>
+ *   <li>Repository is the system default temp directory, as returned by
+ *       <code>System.getProperty("java.io.tmpdir")</code>.</li>
+ * </ul>
+ * <p>
+ * <b>NOTE</b>: Files are created in the system default temp directory with
+ * predictable names. This means that a local attacker with write access to that
+ * directory can perform a TOUTOC attack to replace any uploaded file with a
+ * file of the attackers choice. The implications of this will depend on how the
+ * uploaded file is used but could be significant. When using this
+ * implementation in an environment with local, untrusted users,
+ * {@link #setRepository(File)} MUST be used to configure a repository location
+ * that is not publicly writable. In a Servlet container the location identified
+ * by the ServletContext attribute <code>javax.servlet.context.tempdir</code>
+ * may be used.
+ * </p>
+ * When using this implementation, bear in mind that AES encrypted data are
+ * "malleable". That is, bytes may be changed and will be decrypted accordingly.
+ * See https://en.wikipedia.org/wiki/Malleability_(cryptography) for more information.
+ *
+ * <p>Temporary files, which are created for file items, should be
+ * deleted later on. The best way to do this is using a
+ * {@link FileCleaningTracker}, which you can set on the
+ * {@link EncryptedFileItemFactory}. However, if you do use such a tracker,
+ * then you must consider the following: Temporary files are automatically
+ * deleted as soon as they are no longer needed. (More precisely, when the
+ * corresponding instance of {@link java.io.File} is garbage collected.)
+ * This is done by the so-called reaper thread, which is started and stopped
+ * automatically by the {@link FileCleaningTracker} when there are files to be
+ * tracked.
+ * It might make sense to terminate that thread, for example, if
+ * your web application ends. See the section on "Resource cleanup"
+ * in the users guide of commons-fileupload.</p>
+ *
+ * @see {@code org.apache.commons.fileupload.disk.DiskFileItem}
+ *
+ * @since FileUpload 1.4
  */
 public class EncryptedFileItemFactory implements FileItemFactory {
 
