fa1d30bb86d55c9a94a960ac206819c0c27b1147
David Carboni <david@carboni.io>
2018-05-09 18:53:56 +0100

    Updated package statement.

--- src/main/java/org/apache/commons/fileupload/disk/Cryptography.java
+++ src/main/java/org/apache/commons/fileupload/disk/Cryptography.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.commons.fileupload.encrypted;
+package org.apache.commons.fileupload.disk;
 
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
